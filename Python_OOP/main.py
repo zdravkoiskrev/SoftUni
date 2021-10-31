af9d5947f3802fdfe05875b4371f677ec05bc992
zdravkoiskrev <76886620+zdravkoiskrev@users.noreply.github.com>
2021-10-31 19:50:02 +0200

    Update

--- Python_OOP/main.py
+++ Python_OOP/main.py
@@ -1,16 +1,10 @@
-from project.task import Task
-from project.section import Section
+from project.player import Player
+from project.guild import Guild
 
 
-task = Task("Make bed", "27/05/2020")
-print(task.change_name("Go to University"))
-print(task.change_due_date("28.05.2020"))
-task.add_comment("Don't forget laptop")
-print(task.edit_comment(0, "Don't forget laptop and notebook"))
-print(task.details())
-section = Section("Daily tasks")
-print(section.add_task(task))
-second_task = Task("Make bed", "27/05/2020")
-section.add_task(second_task)
-print(section.clean_section())
-print(section.view_section())
+player = Player("George", 50, 100)
+print(player.add_skill("Shield Break", 20))
+print(player.player_info())
+guild = Guild("UGT")
+print(guild.assign_player(player))
+print(guild.guild_info())
