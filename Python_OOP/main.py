d8845cd3d777326ed205104101ad294dda8ada38
zdravkoiskrev <76886620+zdravkoiskrev@users.noreply.github.com>
2021-11-09 00:51:13 +0200

    Update

--- Python_OOP/main.py
+++ Python_OOP/main.py
@@ -1,10 +1,15 @@
-from project.player import Player
-from project.guild import Guild
+from project.mammal import Mammal
+from project.reptile import Reptile
+from project.animal import Animal
+from project.bear import Bear
+from project.gorilla import Gorilla
+from project.snake import Snake
+from project.lizard import Lizard
 
 
-player = Player("George", 50, 100)
-print(player.add_skill("Shield Break", 20))
-print(player.player_info())
-guild = Guild("UGT")
-print(guild.assign_player(player))
-print(guild.guild_info())
+mammal = Mammal("Stella")
+print(mammal.__class__.__bases__[0].__name__)
+print(mammal.name)
+lizard = Lizard("John")
+print(lizard.__class__.__bases__[0].__name__)
+print(lizard.name)
